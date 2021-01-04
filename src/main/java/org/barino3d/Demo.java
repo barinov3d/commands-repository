954be6a75d683bd30624ef6b0f9e4f8ec7a5fae3
dmitry barinov <barinov3d@gmail.com>
2021-01-04 18:28:44 +0300

    add index.html template page and first controller

---
+++ src/main/java/org/barino3d/Demo.java
@@ -0,0 +1,6 @@
+@SpringBootApplication
+public class Demo {
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(Demo.class);
+    }
+}
---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,8 @@
+function showStuff(cls,btn) {
+    /*btn.querySelector(cls).style.display = "flex";*/
+    btn.querySelector(cls).style.visibility = "visible";
+}
+function hideStuff(cls,btn) {
+    /*btn.querySelector(cls).style.display = "none";*/
+    btn.querySelector(cls).style.visibility = "hidden";
+}
