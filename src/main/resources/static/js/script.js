b41cd8b14d6c63c5773c384088e5f3dbfb558c50
barinov3d@gmail.com <Beketova4215>
2021-01-07 15:49:02 +0300

    update js for buttons appearing

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,12 +1,6 @@
-function showStuff(btn) {
-    document.querySelectorAll('.btn1').forEach(e => e.style.display = "none");
-    document.querySelectorAll('.btn2').forEach(e => e.style.display = "none");
-    btn.querySelector('.btn1').style.display = "flex";
-    btn.querySelector('.btn2').style.display = "flex";
-    let command = btn.querySelector('.command-code');
-    let bgColor = command.style.background;
-/*    command.style.background = "#e7e7e7";
-        setTimeout(function(){
-            command.style.background = bgColor;
-        }, 1000);*/
+function showButtons(codeText) {
+    document.querySelectorAll('.buttons').forEach(e => e.style.display = "none");
+    let closestCommand = codeText.closest('.command');
+    let buttons = closestCommand.querySelector('.buttons');
+    buttons.style.display = "flex";
 }
