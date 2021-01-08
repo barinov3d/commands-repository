60932b2f364cf64bd8881d269cf6373e46c80644
dmitry barinov <barinov3d@gmail.com>
2021-01-08 08:55:54 +0300

    Update CommandController delete method

--- src/main/java/org/barino3d/controllers/CommandController.java
+++ src/main/java/org/barino3d/controllers/CommandController.java
@@ -3,8 +3,8 @@ package org.barino3d.controllers;
 import lombok.AllArgsConstructor;
 import org.barino3d.models.Application;
 import org.barino3d.models.Command;
-import org.barino3d.services.CommandService;
 import org.barino3d.services.ApplicationService;
+import org.barino3d.services.CommandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -18,21 +18,21 @@ public class CommandController {
     private final ApplicationService applicationService;
     private final CommandService commandService;
 
-    @PostMapping("{id}/command")
-    public String addCommand(@PathVariable String id, @ModelAttribute(value = "newCommand") Command command) {
+    @PostMapping("{appId}/command")
+    public String addCommand(@PathVariable String appId, @ModelAttribute(value = "newCommand") Command command) {
         Command newCommand = new Command();
         newCommand.setDescription(command.getDescription());
         newCommand.setText(command.getText());
-        final Application application = applicationService.findById(id);
+        final Application application = applicationService.findById(appId);
         application.getCommands().add(commandService.save(newCommand));
         applicationService.save(application);
-        return "redirect:/" + id;
+        return "redirect:/" + appId;
     }
 
-    @PostMapping("/command/{id}/delete")
-    public String deleteCommand(@PathVariable String id, @ModelAttribute(value = "currentApplication") Application currentApplication) {
-        commandService.delete(commandService.findById(id));
-        return "redirect:/" + currentApplication.getId();
+    @PostMapping("{appId}/command/{cmdId}/delete")
+    public String deleteCommand(@PathVariable String appId, @PathVariable String cmdId) {
+        commandService.delete(commandService.findById(cmdId));
+        return "redirect:/" + appId;
     }
 
 
