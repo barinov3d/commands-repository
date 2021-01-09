8a59d781012ccc160554fe95403527423a36868e
barinov3d@gmail.com <Beketova4215>
2021-01-09 13:22:55 +0300

    Add custom login page

--- src/main/java/org/barino3d/security/SecurityConfiguration.java
+++ src/main/java/org/barino3d/security/SecurityConfiguration.java
@@ -2,12 +2,10 @@ package org.barino3d.security;
 
 import org.barino3d.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -21,11 +19,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests().antMatchers("/login").anonymous()
+                .authorizeRequests().antMatchers("/login*", "/js/**", "/css/**").permitAll()
                 .and()
                 .authorizeRequests().anyRequest().authenticated()
                 .and()
-                .formLogin();
+                .formLogin().loginPage("/login").permitAll();
 
     }
 
